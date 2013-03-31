c9b754bf15e6539b084dc2392bda4ef68be08998
Toshiyuki Tezuka <TSUN@Toshiyuki-Tezuka-no-MacBook-Air.local>
2013-03-31 14:08:13 +0900

    create
    create project

---
+++ Utilities/Utilities/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Utilities
+//
+//  Created by Toshiyuki Tezuka on 2013/03/31.
+//  Copyright (c) 2013年 tsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Utilities/Utilities/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Utilities
+//
+//  Created by Toshiyuki Tezuka on 2013/03/31.
+//  Copyright (c) 2013年 tsun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
